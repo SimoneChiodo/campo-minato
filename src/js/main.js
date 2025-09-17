edce9289ec5de9a1a4b0eaff5970a18262cf1b28
SimoneChiodo <simochi.005@gmail.com>
2025-09-17 17:57:06 +0200

    Add numbers of near bombs

--- src/js/main.js
+++ src/js/main.js
@@ -34,13 +34,16 @@ function cellClick(index) {
   if(bombs.has(index)) {
     // Rivelo le bombe
     bombs.forEach(bomb => {
-      const cell = document.getElementById(`cell-${bomb}`);
+      const cell = document.getElementById(`cell-${bomb}`); // Celle con una bomba
       cell.innerText = "B";
     });
     // Informo che si ha perso
     gameWin = false;
     resultText.innerText = "Hai perso!";
   } else if(gameWin !== false) {
+    const cell = document.getElementById(`cell-${index}`); // Prendo la cella selezionata
+    cell.innerText = revealCell(index); // Mostro la cella
+
     // Se il giocatore ha vinto
     if(clickedCells.size === (cellsNumber - 16)){
       gameWin = true;
@@ -51,6 +54,31 @@ function cellClick(index) {
     clickedCells.add(index);
     scoreText.innerText = `Punteggio: ${clickedCells.size}`;
   }
+}
+
+function revealCell(index) {
+  let nearBombsCounter = 0;
+  let cellsPerLine = Math.sqrt(cellsNumber); // Radice quadrata del numero di celle totali (risultato: celle per linea)
+  
+  // Controllo riga attuale
+  bombs.has(index-1) && nearBombsCounter++;
+  bombs.has(index) && nearBombsCounter++;
+  bombs.has(index+1) && nearBombsCounter++;
+
+  if(index < (cellsNumber-cellsPerLine)) { // Se non è nell'ultima riga
+    let nextLine = index + cellsPerLine; // Controllo riga successiva
+    bombs.has(nextLine-1) && nearBombsCounter++;
+    bombs.has(nextLine) && nearBombsCounter++;
+    bombs.has(nextLine+1) && nearBombsCounter++;
+  }
+  if(index > cellsPerLine) { // Se non è nella prima riga
+    let previousLine = index - cellsPerLine; // Controllo riga precedente
+    bombs.has(previousLine-1) && nearBombsCounter++;
+    bombs.has(previousLine) && nearBombsCounter++;
+    bombs.has(previousLine+1) && nearBombsCounter++;
+  }
+
+  return nearBombsCounter;
 }
 
 // Funzione che inizializza il numero di celle e le mostra correttamente in pagina
